d481b54e62bcce079b762ff809beed848ae93c34
Denitsa Georgieva <denittsaa@yahoo.com>
2021-04-29 16:01:14 -0600

    App Change.

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -2,18 +2,12 @@ import React from 'react'
 import './History.css'
 
 const History = () => {
+  const createNewDiv =  => [...Array()]
+
   return (
     <div className='history-div'>
       <h2 className='text-history'>History</h2>
-      <div className='fist-history-div'>
-        <p className='fist-history-text'></p>
-      </div>
-      <div className='second-history-div'>
-        <p className='second-history-text'> </p>
-      </div>
-      <div className='third-history-div'>
-        <p className='third-history-text'></p>
-      </div>
+      <p className='fist-history-div'></p>
     </div>
   )
 }
