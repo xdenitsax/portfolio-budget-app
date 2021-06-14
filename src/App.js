bbe76b6666417828d775e11d2d2636c42149b015
Denitsa Georgieva <denittsaa@yahoo.com>
2021-06-13 22:57:56 -0600

    Navigation-Bar.
    Navigation-bar.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Register from './components/Pages/Register/Register'
 import LandingPage from './components/Pages/LandingPage/LandingPage'
 import Header from './components/Header/Header'
 import Home from './components/Pages/Home/Home'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.css'
+import NavigationBar from './components/NavigationBar/NavigationBar'
 
 const App = () => {
   return (
     <div className="app-div">
-      <Header />
-      <BrowserRouter>
-        <div>
+      {/* <Header /> */}
+      <Router>
+        <NavigationBar />
+        <Switch>
           <Route path="/" exact component={LandingPage} />
-          <Route path="/signin" component={LoginPage} />
+          <Route path="/login" component={LoginPage} />
           <Route path="/register" component={Register} />
           <Route path="/add-transaction" component={Home} />
-        </div>
-      </BrowserRouter>
+        </Switch>
+      </Router>
     </div>
   )
 }
