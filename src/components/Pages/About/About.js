7cdee516d1d8a8edb81ea3dbdf108da9c1dfa798
Denitsa Georgieva <denittsaa@yahoo.com>
2021-06-15 15:39:40 -0600

    Add Supprot and About Page.

---
+++ src/components/Pages/About/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import NavigationBar from '../../NavigationBar/NavigationBar'
+import { Link } from 'react-router-dom'
+import './About.css'
+const About = () => {
+  return (
+    <div>
+      <div className="background-about">
+        <div className="image">
+          <img className="image-girl" src="./girl-saving.jpg"></img>
+        </div>
+        <div className="text-div">
+          <p className="text">
+            Expense Tracker was founded on a simple, powerful idea: modern family life is hard enough. There has to be a
+            better way. And that is how we live and work every day. How can we make life easier — for everyone? From
+            employees, partners and customers we want to use technology, built for the way we live and work now, to make
+            life easier. This makes for happy customers, happy children, happy workers and overall, a better way to
+            live.
+          </p>
+        </div>
+      </div>
+
+      <div className="div-button">
+        <Link to="/">
+          {' '}
+          <button className="button">Go Back</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default About
